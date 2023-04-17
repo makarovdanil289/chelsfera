a7e2ae960f133f7103a81ea80e0d72754e87a339
Danil <makarovdanil289@gmail.com>
2023-04-17 18:59:58 +0500

    нужно решить проблему с адаптацией. carousel__mobile

--- src/js/index.js
+++ src/js/index.js
@@ -15,3 +15,21 @@ window.addEventListener('DOMContentLoaded', () => {
         })
     })
 })
+
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 300,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/previous.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/next.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+});
