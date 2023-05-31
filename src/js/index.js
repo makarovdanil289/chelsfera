8d6546c9ee0bd0595ce303663f7a49162d6bf243
Danil <makarovdanil289@gmail.com>
2023-05-31 22:52:29 +0500

    was added adaptation on the pages numbers and number_index

--- src/js/index.js
+++ src/js/index.js
@@ -22,4 +22,33 @@ $(document).ready(function() {
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
           .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
       });
+
+    // $('.carousel__inner').slick({
+    //     speed: 300,
+    //     slidesToShow: 1,
+    //     adaptiveHeight: true,
+    //     prevArrow: '<button type="button" class="slick-prev"><img src="../icons/previous.svg"></button>',
+    //     nextArrow: '<button type="button" class="slick-next"><img src="../icons/next.svg"></button>',
+    //     responsive: [
+    //         {
+    //             breakpoint: 1024,
+    //             settings: {
+    //                 slidesToShow: 1,
+    //                 slidesToScroll: 1,
+    //                 infinite: true
+    //             }
+    //         },
+    //         {
+    //           breakpoint: 650,
+    //           settings: {
+    //             dots: true,
+    //             infinite: true,
+    //             speed: 300,
+    //             slidesToShow: 1,
+    //             adaptiveHeight: true,
+    //             arrows:  false
+    //           }
+    //         }
+    //     ]
+    // });
 });
